6cbf8d19946f9821ffd5427c428ce07c29b87ef4
Antoine <antoine.joly@diiage.org>
2021-04-01 12:44:28 +0200

    Add task section

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import Task from "./components/Task";
 
 export default function App() {
@@ -8,12 +15,18 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.tasksSection}>
         <Text style={styles.title}>Todo today</Text>
-        <View style={styles.tasks}>
+        <View>
           <Task text="💪 Work out 30 minutes"></Task>
           <Task text="🏠 Ace the react native test"></Task>
           <Task text="🍸 Chill out"></Task>
         </View>
       </View>
+      <KeyboardAvoidingView style={styles.bottom}>
+        <TextInput style={styles.input} placeholder="Add a task"></TextInput>
+        <TouchableOpacity style={styles.add}>
+          <Text style={styles.addText}>+</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -21,6 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
     backgroundColor: "#8C82FF",
   },
   tasksSection: {
@@ -32,5 +47,33 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#fff",
     marginBottom: 30,
+  },
+  input: {
+    borderRadius: 60,
+    marginBottom: 37,
+    paddingVertical: 15,
+    marginHorizontal: 20,
+    backgroundColor: "#FFF",
+    textAlign: "center",
+    width: "90%",
+  },
+  bottom: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+  },
+  add: {
+    borderRadius: 60,
+    height: 60,
+    width: 60,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 29,
+  },
+  addText: {
+    fontSize: 60,
+    marginTop: -10,
+    color: "#55BCF6",
   },
 });
